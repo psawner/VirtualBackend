555e2c9b59f84163008658036c3331493f764a71
psawner <adventure03ps@gmail.com>
2025-07-02 17:17:44 +0530

    Delete backend directory

--- backend/middleware/ensureAuthenticated.js
+++
@@ -1,8 +0,0 @@
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next(); // ✅ User is authenticated, proceed to next middleware or route
-  }
-  return res.status(401).json({ message: "Unauthorized. Please log in." }); // ❌ Not logged in
-}
-
-module.exports = ensureAuthenticated;
